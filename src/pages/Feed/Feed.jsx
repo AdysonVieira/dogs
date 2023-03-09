c32c47a3ad3f4a59d7a98b9f308c08325b6cc90e
Adyson Vieira <contato.adyson@gmail.com>
2023-03-08 23:00:05 -0300

    Build: Feed Modal

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -8,6 +8,7 @@ import Error from '../../components/Helper/Error';
 import styles from './Feed.module.css';
 const Feed = () => {
   const { data, loading, error, request} = useFetch();
+  const [modalOpened, setModalOpened] = React.useState(false);
 
   const fetchPhotosToFeed = async () => {
     const {url, options} = PHOTOS_GET({page: 1, total: 6, user: 0})
@@ -22,11 +23,18 @@ const Feed = () => {
   if (error) return <Error error={error} />
   if (data) {
     return (
-      <ul className={styles.feed}>
-        {data.map((post) => (
-          <FeedPhotosItem key={post.id} {...post} />
-        ))}
-      </ul>
+      <>
+        {modalOpened && <FeedModal />}
+        <ul className={styles.feed}>
+          {data.map((post) => (
+            <FeedPhotosItem
+              key={post.id}
+              {...post}
+              toggle={setModalOpened}
+              value={modalOpened}/>
+            ))}
+        </ul>
+      </>
     )
   }
 }
--- src/pages/Feed/FeedPhotosItem.jsx
+++ src/pages/Feed/FeedPhotosItem.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styles from './FeedPhotosItem.module.css'
 
 const FeedPhotosItem = (props) => {
+  
+  const handleClick = () => {
+    props.toggle(!props.value)
+  }
+  
   return (
-    <li className={styles.photo}>
+    <li className={styles.photo} onClick={handleClick}>
       <img src={props.src} alt={props.title} />
       <span className={styles.view}>{props.acessos}</span>
     </li>
